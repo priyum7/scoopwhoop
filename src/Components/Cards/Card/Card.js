1cc7c3fcb06227506e60f24f3a7379a9368610d6
priyum7 <priyumagrawal@gmail.com>
2021-05-31 03:49:30 +0530

    Add Cards Layout
    - Add Cards Component : responsible for fetching data from endpoint and structuring cards in a grid layout
    - Add Card Component : responsible for generating individual cards

---
+++ src/Components/Cards/Card/Card.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Card.css";
+
+const getDateSuffix = (publishedDays) => {
+  console.log(publishedDays);
+  if (publishedDays === 1) return `${publishedDays} day ago `;
+  else if (publishedDays <= 6) return `${publishedDays} days ago `;
+  else if (publishedDays < 14)
+    return `${Math.floor(publishedDays / 7)} week ago `;
+  else if (publishedDays < 30)
+    return `${Math.floor(publishedDays / 7)} weeks ago `;
+  else if (publishedDays < 60)
+    return `${Math.floor(publishedDays / 30)} month ago `;
+  else return `${Math.floor(publishedDays / 30)} months ago `;
+};
+
+const baseUrl = "https://www.scoopwhoop.com/";
+const urlSuffix = "?ref=read_fresh";
+
+function Card(props) {
+  return (
+    <div className="card_outer">
+      <div className="article_img">
+        <a href={`${baseUrl}${props.articleUrl}${urlSuffix}`}>
+          <img src={props.imagePath} alt="Article" />{" "}
+        </a>
+      </div>
+      <div className="article_category">
+        <a
+          href={`${baseUrl}category/${props.category.toLowerCase()}/${urlSuffix}`}
+        >
+          {props.category}
+        </a>
+      </div>
+      <div className="article_title">
+        <a href={`${baseUrl}${props.articleUrl}${urlSuffix}`}>
+          <h4>{props.title}</h4>
+        </a>
+      </div>
+      <div className="article_author">
+        <a href={`${baseUrl}author/${props.authorUrl}${urlSuffix}`}>
+          {props.authorName}
+        </a>
+      </div>
+      <div className="article_publishdate_readtime">{`${getDateSuffix(
+        props.publishedDays
+      )}  | 
+      ${props.readTime}`}</div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/Components/Cards/Cards.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Card from "./Card/Card";
+import "./Cards.css";
+function Cards() {
+  const [articles, setArticles] = useState(undefined);
+
+  useEffect(() => {
+    fetch("https://www.scoopwhoop.com/api/v4/read/all/?offset=0&limit=8")
+      .then((res) => res.json())
+      .then((res) => {
+        setArticles(res.data);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  console.log(articles);
+
+  return (
+    <div className="cards_grid">
+      {articles &&
+        articles.map((article) => (
+          <Card
+            key={article.article_id}
+            category={article.category[0].toUpperCase()}
+            imagePath={article.feature_img}
+            title={article.title}
+            authorName={article.auth_display.display_name}
+            publishedDays={
+              new Date().getDate() - new Date(article.pub_date).getDate() - 1
+            }
+            readTime={article.readtime}
+            articleUrl={article.slug}
+            authorUrl={article.auth_display.author_url}
+          />
+        ))}
+    </div>
+  );
+}
+
+export default Cards;

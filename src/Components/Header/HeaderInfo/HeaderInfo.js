c03aefc8cc137909e23402509f8df4df21a5d725
priyum7 <priyumagrawal@gmail.com>
2021-06-02 13:24:21 +0530

    Add Minor Style Fix
    -Correct arrow SVG color
    -Add roboto font

--- src/Components/Header/HeaderInfo/HeaderInfo.js
+++ src/Components/Header/HeaderInfo/HeaderInfo.js
@@ -12,7 +12,7 @@ function HeaderInfo() {
         </div>
         <div className="headertile_readmore">
           <a href="https://www.scoopwhoop.com/entertainment/movies-that-were-copied/?ref=read_masthead">
-            Read Article <ArrowForwardIcon />
+            Read Article <ArrowForwardIcon style={{ color: "#9f49e3" }} />
           </a>
         </div>
       </div>
